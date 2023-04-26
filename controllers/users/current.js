f407c26613139e41dcbdf59905ab8b1185d37032
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2023-04-26 20:35:57 +0300

    Returned an extended response.

--- controllers/users/current.js
+++ controllers/users/current.js
@@ -3,13 +3,17 @@ const { catchAsync } = require("../../utils");
 const current = catchAsync(async (req, res, next) => {
   const { name, email, avatarURL, birthday, phone, telegram } = req.user;
   res.json({
-    user: {
-      name,
-      email,
-      avatarURL,
-      birthday,
-      phone,
-      telegram,
+    status: "success",
+    code: 200,
+    data: {
+      user: {
+        name,
+        email,
+        avatarURL,
+        birthday,
+        phone,
+        telegram,
+      },
     },
   });
 });
